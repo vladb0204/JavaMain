8d86920890a75940970467daeb7bc04a266e46bd
Yury Yartsev <yury-yartsev@yandex.ru>
2020-09-28 20:25:53 +0300

    The first commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int num = sc.nextInt();
+        int[] nums = new int[num];
+
+        for (int i = 0; i < num; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        Arrays.sort(nums);
+
+        System.out.println(nums[nums.length-1]);
+    }
+}
