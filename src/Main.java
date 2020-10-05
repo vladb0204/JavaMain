dfed628810338723255b6cb5be82887614df2aac
vladb0204 <vladbalandin2013@gmail.com>
2020-10-05 13:00:34 +0300

    5th October 2020

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,97 @@
 import java.util.*;
-import java.lang.*;
+
+
 
 public class Main {
-    public static void main(String[] args) {
+
+    // Task 1
+
+    public static void main_1(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int num = sc.nextInt();
-        int[] nums = new int[num];
+        int a = sc.nextInt();
+        int b = sc.nextInt();
 
-        for (int i = 0; i < num; i++) {
-            nums[i] = sc.nextInt();
+        int[][] matrix = new int[a][b];
+        for (int i = 0; i < a; i++) {
+            for (int j = 0; j < b; j++) {
+                matrix[i][j] = sc.nextInt();
+            }
         }
 
-        Arrays.sort(nums);
-        //Vlados lox
-        System.out.println(nums[nums.length-1]);
+        int[][] matrix_1 = new int[b][a];
+        for (int i = 0; i < a; i++) {
+            for (int j = 0; j < b; j++) {
+                matrix_1[j][a-i-1] = matrix[i][j];
+            }
+        }
+
+        System.out.println(b + " " + a);
+
+        for (int i = 0; i < b; i++) {
+            for (int j = 0; j < a; j++) {
+                System.out.print(matrix_1[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    // Task 2
+
+    public static void main_2(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+
+        int[][] matrix = new int[a][a];
+        for (int i = 0; i < a; i++) {
+            for (int j = 0; j < a; j++) {
+                if (i + j > a - 1) {
+                    matrix[i][j] = 2;
+                } else if (i + j == a - 1) {
+                    matrix[i][j] = 1;
+                } else if (i + j < a - 1) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        for (int i = 0 ; i < a; i++) {
+            for (int j = 0; j < a; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    // Task 3
+
+    public static void main_3(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        boolean result = true;
+
+        int[][] matrix = new int[a][a];
+        for (int i = 0; i < a; i++) {
+            for (int j = 0; j < a; j++) {
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+
+        for (int i = 0; i < a; i++) {
+            for (int j = 0; j < a; j++) {
+                if (matrix[i][j] != matrix[j][i]) {
+                    result = false;
+                    break;
+                }
+            }
+        }
+
+        if (result) {
+            System.out.println("yes");
+        } else {
+            System.out.println("no");
+        }
     }
 }
